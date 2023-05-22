d8527d8f9dbc7476ee2d86a5583871df56a728c2
Emma Ton <anh_ton@college.harvard.edu>
2023-05-22 19:06:34 -0400

    change to ChatCompletion for multi-turn convo

--- main.py
+++ main.py
@@ -1,37 +1,36 @@
 import openai
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv('.env')
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 def reply(user_input):
     if not user_input:
-        return "Please provide a valid input."
+        return "Error: input can't be empty"
     try: 
-        res = openai.Completion.create(
-            model = "text-davinci-003",
-            prompt = user_input,
-            max_tokens = 100,
-            temperature = 0.01
-        ).choices[0].text.strip()
+        res = openai.ChatCompletion.create(
+            model = "gpt-3.5-turbo-0301",
+            messages = [{"role": "user", "content": user_input}]
+        ).choices[0].message.content.strip()
         return res
     except Exception as e:
-        return "An error occurred: " + str(e)
+        return "Error: " + str(e)
     
 
 def main():
 
+    st.title("Chatbot with Streamlit")
+    st.markdown("Welcome to the Chatbot. Ask me anything!")
+
+    user_input = st.text_input("You:")
+    if st.button("Send"):
+        response = reply(user_input)
+        st.text("Chatbot: " + response)
+
+
     print("Welcome to the Chatbot. Ask me anything or type #quit to exit")
-    
-    while True:
-        user_input = input("You: ")
-        if user_input == '#quit':
-            break
-        
-        print("Chatbot:", reply(user_input))
 
 if __name__ == '__main__':
     main()
-
-
