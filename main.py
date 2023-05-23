0bdc0141a32ad3c9772d779e10504b6dd6bbc669
Emma Ton <anh_ton@college.harvard.edu>
2023-05-23 12:51:15 -0400

    delete unused lib

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import os
 from dotenv import load_dotenv
 import streamlit as st
 from streamlit_chat import message
-import time
 
 load_dotenv('.env')
 openai.api_key = os.getenv('OPENAI_API_KEY')
