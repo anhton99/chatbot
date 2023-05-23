d94a7e656993ea56f76fd582ae09e0fec5b1b446
Emma Ton <anh_ton@college.harvard.edu>
2023-05-22 20:12:48 -0400

    add statemanagement and streamlit_chat

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import openai
 import os
 from dotenv import load_dotenv
 import streamlit as st
+from streamlit_chat import message
+import time
 
 load_dotenv('.env')
 openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -22,15 +24,26 @@ def reply(user_input):
 def main():
 
     st.title("Chatbot with Streamlit")
-    st.markdown("Welcome to the Chatbot. Ask me anything!")
+    st.markdown("Welcome to the Chatbot. Ask me anything") 
 
-    user_input = st.text_input("You:")
-    if st.button("Send"):
-        response = reply(user_input)
-        st.text("Chatbot: " + response)
+    if 'answers' not in st.session_state:
+        st.session_state['answers'] = []
+    if 'questions' not in st.session_state:
+        st.session_state['questions'] = []
+
+    user_input = st.text_input("Enter your question here:\n", key = 'input')
+    if user_input:
+        res = reply(user_input)
+        st.session_state['questions'].append(user_input)
+        st.session_state['answers'].append(res)
+        
+    if st.session_state['answers']:
+        for i in range(len(st.session_state['answers'])):
+            message(st.session_state['questions'][i], is_user=True, key=str(i) + '_user')
+            message(st.session_state["answers"][i], key=str(i))
 
 
-    print("Welcome to the Chatbot. Ask me anything or type #quit to exit")
+       
 
 if __name__ == '__main__':
     main()
